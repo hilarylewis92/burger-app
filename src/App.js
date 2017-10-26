b347977a6b96bf56188cba4325cfec0979f7a6a4
Hilary Lewis <hilarylewis92@gmail.com>
2017-10-26 11:14:23 -0600

    Create aux file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
